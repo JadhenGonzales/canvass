44ee6fb27838059a36d46bfa7cb7ffd8e4925f98
Jadhen Nicolo Gonzales <jadhen.g@gmail.com>
2023-10-25 20:27:05 +0800

    removed content script

--- content_scripts/CollectData.js
+++
@@ -1,8 +0,0 @@
-function handleMessages(message, sender, sendResponse) {
-    if (message.type == "autofill") {
-        document.body.innerHTML = "test";
-        sendResponse("test2");
-    }
-}
-
-browser.runtime.onMessage.addListener(handleMessages);
